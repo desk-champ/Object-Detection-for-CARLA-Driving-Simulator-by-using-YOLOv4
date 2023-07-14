914d397e7bdaf24ca7bf9d3b3e69ad2ef7d777e4
desk-champ <lz.115@outlook.com>
2023-07-13 20:53:56 -0500

    updated folders

--- voc2yolo4.py
+++ voc2yolo4.py
@@ -1,8 +1,8 @@
 import os
 import random 
  
-xmlfilepath=r'./VOCdevkit/VOC2007/Annotations'
-saveBasePath=r"./VOCdevkit/VOC2007/ImageSets/Main/"
+xmlfilepath=r'D:/Carla-Object-Detection-Dataset/labels/test'
+saveBasePath=r"D:/Carla-Object-Detection-Dataset/image_sets/testing"
  
 trainval_percent=1
 train_percent=1
